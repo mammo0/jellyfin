6436dea8e1bee35daf8ecd4ee9dba76876a7ced6
SenorSmartyPants <senorsmartypants@gmail.com>
2023-03-13 16:31:55 -0500

    Make sure episodes in series folder don't group as multiversion
    Test for episodes in series folder
    
    Turn on multiple versions for episodes
    
    Support for multiversion episodes in mixed folders
    
    Update 2 failing test cases. These were for passing for unofficially supported filenames. Dashes with no spaces, which is not how JF docs say multiversion files are supposed to be named.
    Fix possible null
    
    fix null take 2
    
    Don't ParseName when calling ResolveVideos<Episode>
    
    ParseName drops everything after year in a filename. This results in episode information being dropped if year is present.
    
    Update tests to set ParseName=false
    
    Additional test with Mixed folder with Year in filename
    
    Added case when calculating displayname for versions for mixed folders.
    Add StringComparison.Ordinal to LastIndexOf
    
    Was generating an error in recent build attempts.
    Clean the episode filename to set the grouper value
    
    This allows files like
    Name (2020) - S01E01 [BluRay-480p x264][AC3 2.0] - [ORIGINAL].mkv
    Name (2020) - S01E01 [BluRay-1080p x264][AC3 5.1]- [Remaster].mkv
    
    to be grouped on 'Name (2020) - S01E01'
    Fix false positive merging
    
    Only do cleanstring or " -" index cleaning, not both.
    Compatiblity fix when stacking episodes and multiple versions are present
    
    Fix linting problems
    
    (cherry picked from commit 89c067c64da80dc2b0f6feab86b9267ecce9c7a1)

--- Emby.Naming/Common/NamingOptions.cs
+++ Emby.Naming/Common/NamingOptions.cs
@@ -739,6 +739,12 @@ namespace Emby.Naming.Common
                 @"^\s*(?<name>[^ ].*?)\s*$"
             };
 
+            VideoVersionExpressions = new[]
+            {
+                // get filename before final space-dash-space
+                @"^(?<filename>.*?)(?:\s-\s(?!.*\s-\s)(.*))?$"
+            };
+
             MultipleEpisodeExpressions = new[]
             {
                 @".*(\\|\/)[sS]?(?<seasonnumber>[0-9]{1,4})[xX](?<epnumber>[0-9]{1,3})((-| - )[0-9]{1,4}[eExX](?<endingepnumber>[0-9]{1,3}))+[^\\\/]*$",
@@ -865,6 +871,11 @@ namespace Emby.Naming.Common
         public string[] CleanStrings { get; set; }
 
         /// <summary>
+        /// Gets or sets list of raw clean strings regular expressions strings.
+        /// </summary>
+        public string[] VideoVersionExpressions { get; set; }
+
+        /// <summary>
         /// Gets or sets list of multi-episode regular expressions.
         /// </summary>
         public EpisodeExpression[] MultipleEpisodeExpressions { get; set; }
@@ -885,12 +896,18 @@ namespace Emby.Naming.Common
         public Regex[] CleanStringRegexes { get; private set; } = Array.Empty<Regex>();
 
         /// <summary>
+        /// Gets list of video version regular expressions.
+        /// </summary>
+        public Regex[] VideoVersionRegexes { get; private set; } = Array.Empty<Regex>();
+
+        /// <summary>
         /// Compiles raw regex strings into regexes.
         /// </summary>
         public void Compile()
         {
             CleanDateTimeRegexes = CleanDateTimes.Select(Compile).ToArray();
             CleanStringRegexes = CleanStrings.Select(Compile).ToArray();
+            VideoVersionRegexes = VideoVersionExpressions.Select(Compile).ToArray();
         }
 
         private Regex Compile(string exp)
